2d6982f82c6e4aba30ff029d061c7905cbbc7a1f
Brian Richardson <brianrichardson@Brians-MacBook-Pro.local>
2023-01-27 11:52:34 -0600

    working draft

--- features/pages/login_page/login_page.py
+++ features/pages/login_page/login_page.py
@@ -5,12 +5,14 @@ class LoginPage:
     def __init__(self, context):
         self.context = context
 
-    def login(self, user_type):
+    def set_login_vals(self, user_type):
         user = self.context.yaml_utils.get_user_info(user_type)
         username_input = self.context.browser.find_element(By.ID, 'user-name')
         password_input = self.context.browser.find_element(By.ID, 'password')
-        login_btn = self.context.browser.find_element(By.ID, 'login-button')
 
         username_input.send_keys(user['username'])
         password_input.send_keys(user['password'])
+
+    def submit_login(self):
+        login_btn = self.context.browser.find_element(By.ID, 'login-button')
         self.context.browser_methods.click(login_btn)
--- features/steps/login_steps/login_steps.py
+++ features/steps/login_steps/login_steps.py
@@ -3,4 +3,5 @@ from behave import *
 
 @step('the user logs in as the "{user_type}"')
 def step_impl(context, user_type):
-    context.login_page.login(user_type)
+    context.login_page.set_login_vals(user_type)
+    context.login_page.submit_login()
